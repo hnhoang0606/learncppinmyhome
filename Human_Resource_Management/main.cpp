ef0cc94a445bf3f0a10f1a2bb35f9f690ec6ea46
hnhoang0606@gmail.com <hnhoang0606.com>
2020-12-20 20:38:28 +0700

    create file

---
+++ Human_Resource_Management/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "businessobject/Employee.h"
+using namespace std;
+
+int main(){
+    cout << "hello" << endl;
+    Employee e(1, "Chau", "Bao", "Ngo", 111222333, "1975-04-30", "Chicago", 'M', 10000, -1, 5);
+    system("pause");
+    return 0;
+}
+
